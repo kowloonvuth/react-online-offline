fa0da2cbbea6793ab83c591d644b8d49cff19d7b
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-07-05 08:43:33 +0700

    using useSyncExternalStore in the onlinestatus hook

--- src/components/useOnlineStatus.js
+++ src/components/useOnlineStatus.js
@@ -1,21 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useSyncExternalStore } from "react";
 
-export function useOnlineStatus(){
-    const[isOnline, setIsOnline] = useState(true);
-    useEffect(() => {
-        function handleOnline(){
-            setIsOnline(true);
-        }
-        function handleOffline(){
-            setIsOnline(false);
-        }
-        window.addEventListener('online', handleOnline);
-        window.addEventListener('offline', handleOffline);
+function subcribe(callback) {
+    window.addEventListener('online', callback);
+    window.addEventListener('offline', callback);
 
-        return() => {
-            window.removeEventListener('online', handleOnline);
-            window.removeEventListener('offline', handleOffline);
-        };
-    }, []);
-    return isOnline;
+    return() => {
+        window.removeEventListener('online', callback);
+        window.removeEventListener('offline', callback);
+    };
+}
+
+export function useOnlineStatus() {
+    return useSyncExternalStore(
+        subcribe,
+        () => navigator.onLine,
+        () => true
+    );
 }
